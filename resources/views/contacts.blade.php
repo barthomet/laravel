40154fb63e28a6c2ec280233eb0dc7117429defd
barthomet <rafabarth97@gmail.com>
2022-02-10 18:36:46 +0000

    teste

---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,2 @@
+<h1>essa é a pagina de contatos</h1>
+<a href="/">Voltar para home</a>
---
+++ resources/views/products.blade.php
@@ -0,0 +1,4 @@
+<h1>essa é a pagina de produtos</h1>
+<a href='/'>Voltar a home page</a>
+<h2>sambiquira por R$2,50 o kl</h2>
+<a href='/contacts'>Fale conosco</a>
